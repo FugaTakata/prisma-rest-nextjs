2194cff28dab22589b82af2ed887350a295fd012
FugaTakata <maple1213goo@gmail.com>
2021-07-14 16:37:17 +0900

    update

--- pages/api/ls.js
+++ pages/api/ls.js
@@ -1,9 +1,10 @@
 const exec = require("child_process").execSync;
 
 export default async function handle(req, res) {
-  const here = exec("ls ./").toString();
-  const parent = exec("ls ../").toString();
-  const grandParent = exec("ls ../../").toString();
+  const here = exec("ls ./").toString().split("\n");
+  const parent = exec("ls ../").toString().split("\n");
+  const grandParent = exec("ls ../../").toString().split("\n");
+  const grandGrandParent = exec("ls ../../../").toString().split("\n");
 
-  res.json({ here, parent, grandParent });
+  res.json({ here, parent, grandParent, grandGrandParent });
 }
