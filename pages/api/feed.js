44b04db981d3f52c5338d2cf56fcfb16e4cf8c03
FugaTakata <maple1213goo@gmail.com>
2021-07-14 11:25:10 +0900

    update a

--- pages/api/feed.js
+++ pages/api/feed.js
@@ -1,7 +1,7 @@
 import prisma from "../../lib/prisma";
+const db = require("../../prisma/dev.db");
 
 export default async function handle(req, res) {
-  console.log("__dirname", __dirname, "process.cwd()", process.cwd());
   const posts = await prisma.post.findMany({
     where: { published: true },
     include: { author: true },
