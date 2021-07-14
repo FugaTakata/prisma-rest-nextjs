f08ebb2c2c7a9ab4f1917b364db5d4f79a043aea
FugaTakata <maple1213goo@gmail.com>
2021-07-14 13:58:20 +0900

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,43 +1,7 @@
 import Layout from "../components/Layout";
-import Post from "../components/Post";
 
-const Blog = (props) => {
-  return (
-    <Layout>
-      <div className="page">
-        <h1>My Blog</h1>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
-        </main>
-      </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
-    </Layout>
-  );
-};
-
-export const getServerSideProps = async () => {
-  const res = await fetch("https://prisma-rest-nextjs.vercel.app/api/feed");
-  const feed = await res.json();
-  return {
-    props: { feed },
-  };
+const Blog = () => {
+  return <Layout>hello</Layout>;
 };
 
 export default Blog;
