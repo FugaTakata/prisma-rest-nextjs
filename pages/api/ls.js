ab4cf916636b686c29f267c692eacf5262c98e56
FugaTakata <maple1213goo@gmail.com>
2021-07-14 17:05:59 +0900

    hello

--- pages/api/ls.js
+++ pages/api/ls.js
@@ -14,6 +14,6 @@ export default async function handle(req, res) {
     grandParent: { dir: __dirname, grandParent },
     grandGrandParent: { dir: __dirname, grandGrandParent },
     parentDb: { dir: __dirname, parentDb },
-    // parentDbS: { dir: __dirname, parentDbS },
+    parentDbS: { dir: __dirname, parentDbS },
   });
 }
