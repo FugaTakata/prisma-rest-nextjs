cf4365365a7328d132292beddeb4fd9f8c394d4a
FugaTakata <maple1213goo@gmail.com>
2021-07-14 16:41:26 +0900

    hello

--- pages/api/ls.js
+++ pages/api/ls.js
@@ -6,5 +6,10 @@ export default async function handle(req, res) {
   const grandParent = exec("ls ../../").toString().split("\n");
   const grandGrandParent = exec("ls ../../../").toString().split("\n");
 
-  res.json({ here, parent, grandParent, grandGrandParent });
+  res.json({
+    here: { dir: __dirname, here },
+    parent: { dir: __dirname, parent },
+    grandParent: { dir: __dirname, grandParentg },
+    grandGrandParent: { dir: __dirname, grandGrandParent },
+  });
 }
