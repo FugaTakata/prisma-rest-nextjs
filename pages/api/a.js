c3e95aef16de48baab934127d55180d2910fe635
FugaTakata <maple1213goo@gmail.com>
2021-07-14 15:04:45 +0900

    update

--- pages/api/a.js
+++ pages/api/a.js
@@ -1,7 +1,11 @@
+const path = require("path");
 import prisma from "../../lib/prisma";
 
 export default async function handle(req, res) {
   const posts = process.env.NEXT_PUBLIC_DATABASE_URL;
   console.log(posts);
-  res.json({ posts });
+  const resultPosts = path.resolve("../../prisma/dev.db");
+  console.log(resultPosts);
+  console.log(__dirname);
+  res.json({ posts, resultPosts, dir: __dirname });
 }
