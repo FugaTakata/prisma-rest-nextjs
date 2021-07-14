892f0aa5def03cd412864555c95dff2e816ee849
FugaTakata <maple1213goo@gmail.com>
2021-07-14 14:22:54 +0900

    update fro

--- pages/api/a.js
+++ pages/api/a.js
@@ -2,5 +2,6 @@ import prisma from "../../lib/prisma";
 
 export default async function handle(req, res) {
   const posts = process.env.NEXT_PUBLIC_DATABASE_URL;
-  res.json(posts);
+  console.log(posts);
+  res.json({ posts });
 }
