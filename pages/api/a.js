290851ad7584c45d9182357831bb902ed9396a54
FugaTakata <maple1213goo@gmail.com>
2021-07-14 16:16:49 +0900

    update

--- pages/api/a.js
+++ pages/api/a.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 import prisma from "../../lib/prisma";
 
 export default async function handle(req, res) {
@@ -7,5 +8,17 @@ export default async function handle(req, res) {
   const resultPosts = path.resolve("../../prisma/dev.db");
   console.log(resultPosts);
   console.log(__dirname);
+
+  if (fs.existsSync(resultPosts)) {
+    console.log("../../prisma/dev.dbある");
+  } else {
+    console.log("../../prisma/dev.dbない");
+  }
+  if (fs.existsSync("/var/task/.next/server/pages/api/dev.db")) {
+    console.log("/var/task/.next/server/pages/api/dev.dbある");
+  } else {
+    console.log("/var/task/.next/server/pages/api/dev.dbない");
+  }
+
   res.json({ posts, resultPosts, dir: __dirname });
 }
